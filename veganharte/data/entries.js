18cc31a461bbaaea8a6d2a785471c888cc311c29
Darío Scattolini <dario.scattolini@gmail.com>
2020-01-17 12:45:00 +0100

    feat: add sample entries data and pages to start testing dinamically served content

---
+++ veganharte/data/entries.js
@@ -0,0 +1,74 @@
+const entries = [
+    {
+        /*----------- MODELO ---------------------------
+        date: "2020-01-16",
+        title: "Torta de zanahoria sin gluten con cobertura de crema de coco",
+            //ver si coincide con metadato HTML, y si puede ser igual que primer párrafo
+        description: "Una deliciosa variante de la clásica carrot cake apta para personas celíacas y con intolerancia al gluten. Ideal como postre o para acompañar un té.",
+        type: "receta", //si no: "articulo"
+        category: "receta", //otras: nutrición, veganismo, barcelona
+        mainTopic: "torta de zanahoria",
+        tags: ["zanahoria", "torta", "merienda", "postre", "té", "sin gluten", "celíacos"],
+        URL: "2020-01-16-receta-torta-de-zanahoria-sin-gluten-con-cobertura-de-crema-de-coco.html", //autogenerado
+        thumbnail: {
+            large: "2020-01-2016-torta-de-zanahoria-thumbnail-large.jpg", //autogenerado
+            small: "2020-01-2016-torta-de-zanahoria-thumbnail-small.jpg", //autogenerado
+        },
+        images: [   //autogenerado
+            {
+                url: "2020-01-2016-algunos-ingredientes-de-la-torta-de-zanahoria.jpg",
+                alt: "Algunos ingredientes de la torta de zanahoria: zanahorias, harina sin gluten, leche vegetal, azúcar."
+            }
+        ],
+        ingredients: ["zanahoria", "harina sin gluten", "leche vegetal", "aceite de coco", "azúcar"]
+    }, ----------------------------------------------*/
+    {
+        date: "2020-01-05",
+        title: "Fajitas de vegetales",
+        description: "Una combinación de vegetales y salsas veganas únicas para rellenar fajitas de estilo mexicano",
+        type: "",
+        category: "",
+        mainTopic: "",
+        tags: [],
+        URL: "2020-01-05-fajitas-de-vegetales.html",
+        thumbnail: {
+            large: "2020-01-05-fajitas-de-vegetales-large.jpg",
+            small: "2020-01-05-fajitas-de-vegetales-small.jpg",
+        },
+        images: [],
+        ingredients: []
+    },
+    {
+        date: "2020-01-12",
+        title: "Tapas veganas",
+        description: "Una selección de platillos veganos para acompañar las cervezas de la tardecita",
+        type: "",
+        category: "",
+        mainTopic: "",
+        tags: [],
+        URL: "2020-01-12-tapas-veganas.html",
+        thumbnail: {
+            large: "2020-01-12-tapas-veganas-large.jpg",
+            small: "2020-01-12-tapas-veganas-small.jpg",
+        },
+        images: [],
+        ingredients: []
+    },
+    {
+        date: "2020-01-16",
+        title: "Ensalada de kale",
+        description: "Una deliciosa y nutritiva ensalada de kale para acompañar cualquier plato principal",
+        type: "",
+        category: "",
+        mainTopic: "",
+        tags: [],
+        URL: "2020-01-16-ensalada-de-kale.html",
+        thumbnail: {
+            large: "2020-01-16-ensalada-de-kale-large.jpg",
+            small: "2020-01-16-ensalada-de-kale-small.jpg",
+        },
+        images: [],
+        ingredients: []
+    }
+    
+];
